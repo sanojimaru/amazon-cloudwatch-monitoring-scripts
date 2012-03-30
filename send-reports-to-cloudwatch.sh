f84a2718636183ed928353da63694933a759cf16
ICCOM Inc <admin@ic-com.co.jp>
2012-03-30 10:46:56 +0900

    add some metrics.

--- send-reports-to-cloudwatch.sh
+++ send-reports-to-cloudwatch.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 DIR=$(cd $(dirname $0); pwd)
+PUT_SCRIPT="aws-scripts-mon/mon-put-instance-data.pl"
 
-$DIR/aws-scripts-mon/mon-put-instance-data.pl \
-  --aws-access-key-id=$AWS_ACCESS_KEY_ID --aws-secret-key=$AWS_SECRET_KEY \
-  --disk-path=/ --disk-space-util --disk-space-avail \
-  --mem-util --mem-avail
+KEY="--aws-access-key-id=${AWS_ACCESS_KEY_ID} --aws-secret-key=${AWS_SECRET_KEY}"
+MEM="--mem-util --mem-avail --mem-used --memory-units=megabytes"
+SWAP="--swap-util --swap-used "
+DISK="--disk-space-units=megabytes "
+
+MOUNT_POINTS=`df -T --exclude-type=tmpfs --exclude-type=devtmpfs | grep /dev/sd | sed -e "s/^.\+[ ]\+\(\/[^ ]*\)$/\1/g"`
+for PATH in $MOUNT_POINTS
+do
+  DISK="--disk-path=${PATH} --disk-space-util --disk-space-used --disk-space-avail ${DISK}"
+done
+OPTIONS="${KEY} ${MEM} ${SWAP} ${DISK}"
+echo $OPTIONS;
+$DIR/$PUT_SCRIPT $OPTIONS;
+
+#   \
+#  --disk-path=/ --disk-space-util --disk-space-avail \
+#  --disk-path=/var/lib/wapps --disk-space-util --disk-space-avail \
+#  --mem-util --mem-avail
   
